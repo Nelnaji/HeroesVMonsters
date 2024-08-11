5215356e775666db63516b512580d4de07833fb6
naofel <nawfal.elkhaznagi@gmail.com>
2024-08-11 22:20:15 +0200

    added bonus modifier

---
+++ src/utils/Utils.java
@@ -0,0 +1,25 @@
+package utils;
+public class Utils {
+
+    public static class Modifier {
+
+        public static int modifier(int baseModifier) {
+
+            int modifierBonus;
+
+            if (baseModifier < 5) {
+                return modifierBonus = -1;
+            } else if (baseModifier < 10) {
+                return modifierBonus = 0;
+            } else if (baseModifier < 15) {
+                return modifierBonus = 1;
+            } else {
+                return modifierBonus = 2;
+            }
+
+        }
+
+
+    }
+
+}
